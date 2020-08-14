fb78325feaf1e2759b99630a9f948b8ce7b6ec2a
Damian <damiandanielvigo@gmail.com>
2020-08-14 10:16:21 -0300

    test-api

--- pages/timeline/index.js
+++ pages/timeline/index.js
@@ -1,9 +1,9 @@
 import Link from 'next/link';
 
-export default function Timeline() {
+export default function Timeline({ userName }) {
   return (
     <>
-      <h1>This is the timeline</h1>
+      <h1>This is the timeline of {userName}</h1>
       <Link href='/'>
         <a>Go home</a>
       </Link>
@@ -16,3 +16,13 @@ export default function Timeline() {
     </>
   );
 }
+
+Timeline.getInitialProps = () => {
+  return fetch('http://localhost:3000/api/hello')
+    .then((res) => res.json())
+    .then((response) => {
+      console.log(response);
+      const { userName } = response;
+      return { userName };
+    });
+};
